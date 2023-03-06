a5e965c95e6b7d0ee26f917640054e4eb2efdb12
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-03-06 12:29:06 +0000

    functions

--- app.ts
+++ app.ts
@@ -6,5 +6,18 @@ function printResult(num: number) {
   console.log(num);
 }
 
-let combinedValues: any;
-combinedValues = add;
+function addandhandle(
+  n1: number,
+  n2: number,
+  cb: (a: number) => void
+) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+console.log(combineValues(2, 3));
+addandhandle(2, 3, (result) => {
+  console.log(result);
+});
