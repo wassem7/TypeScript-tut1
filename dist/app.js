a75adf9a073b67bc12a428bcd58b284cc5ca7be1
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-03-15 05:36:42 +0000

    Generic type constraints

--- dist/app.js
+++ dist/app.js
@@ -6,3 +6,8 @@ const promise = new Promise((resolve, reject) => {
     }, 2000);
 });
 promise.then((data) => console.log(data));
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const a = merge({ name: 'Wassem' }, 3);
+console.log(a);
--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,17 @@ const promise: Promise<string> = new Promise(
 );
 
 promise.then((data) => console.log(data));
+
+function merge<T extends Object, U extends Object>(
+  objA: T,
+  objB: U
+) {
+  return Object.assign(objA, objB);
+}
+
+// console.log(
+//   merge({ name: 'Wassem' }, { networth: 'unlimited' })
+// );
+
+const a = merge({ name: 'Wassem' }, 3);
+console.log(a);
