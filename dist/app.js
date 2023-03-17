399a7b118efae1ce025dbc69be1871a6323e0999
Wassem Darkwa <waseemdarkwa@gmail.com>
2023-03-17 09:23:20 +0000

    Decorator functions

--- dist/app.js
+++ dist/app.js
@@ -1,41 +1,22 @@
-'use strict';
-var __decorate =
-  (this && this.__decorate) ||
-  function (decorators, target, key, desc) {
-    var c = arguments.length,
-      r =
-        c < 3
-          ? target
-          : desc === null
-          ? (desc = Object.getOwnPropertyDescriptor(target, key))
-          : desc,
-      d;
-    if (
-      typeof Reflect === 'object' &&
-      typeof Reflect.decorate === 'function'
-    )
-      r = Reflect.decorate(decorators, target, key, desc);
-    else
-      for (var i = decorators.length - 1; i >= 0; i--)
-        if ((d = decorators[i]))
-          r =
-            (c < 3
-              ? d(r)
-              : c > 3
-              ? d(target, key, r)
-              : d(target, key)) || r;
-    return (
-      c > 3 && r && Object.defineProperty(target, key, r), r
-    );
-  };
-function Logger(target) {
-  console.log('Logging ....');
-  console.log(target);
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(logstring) {
+    return function (target) {
+        console.log(logstring);
+        console.log(target);
+    };
 }
 let Person = class Person {
-  constructor() {
-    this.name = 'Wassem';
-    console.log('Creating person object ...');
-  }
+    constructor() {
+        this.name = 'Wassem';
+        console.log('Creating person object ...');
+    }
 };
-Person = __decorate([Logger], Person);
+Person = __decorate([
+    Logger('LOGGING STRING !!')
+], Person);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-function Logger(target: Function) {
-  console.log('Logging ....');
-  console.log(target);
+// function Logger(target: Function) {
+//   console.log('Logging ....');
+//   console.log(target);
+// }
+
+function Logger(logstring: string) {
+  return function (target: Function) {
+    console.log(logstring);
+    console.log(target);
+  };
 }
-@Logger
+@Logger('LOGGING STRING !!')
 class Person {
   name: string = 'Wassem';
 
